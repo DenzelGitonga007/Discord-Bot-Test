627a4f4bb4e8df0766edaa8ff0705e277fc64ef7
Denzel Gitonga <gitongadenzel@gmail.com>
2023-09-03 18:28:44 +0300

    refactor

--- bot.py
+++ bot.py
@@ -1,18 +1,35 @@
+# Import necessary libraries
+import os
+from dotenv import load_dotenv
 import discord
 from discord.ext import commands
 
-# Create a bot instance with intents
+# Load environment variables from a .env file
+load_dotenv()
+# The intents
 intents = discord.Intents.default()
-intents.typing = False  # Disable typing events to reduce unnecessary data
-bot = commands.Bot(command_prefix='/', intents=intents)
+intents.typing = False
+# Set up the bot command prefix
+bot = commands.Bot(command_prefix='', intents=intents)
 
+# Event handler for when the bot is ready
 @bot.event
 async def on_ready():
-    print(f'Logged in as {bot.user.name}')
+    print("Bot '{}' is online...".format(bot.user.name))
 
-@bot.command()
+# Event handler for when a message is received
+@bot.event
+async def on_message(message):
+    # Convert the message content to lowercase
+    message.content = message.content.lower()
+    
+    # Process the message as needed
+    await bot.process_commands(message)
+
+# Responds to !hello
+@bot.command(name='hello', help='Say hello to the bot')
 async def hello(ctx):
-    await ctx.send('Hello!')
+    await ctx.send("Hello {}".format(ctx.author.mention))
 
-# Bot's actual token
-# bot.run('MTE0Njc4NDA2MTg4MTMzNTkzOA.GWox_K.dPHnK-h3-CKmjXozyyUczkHsJedmxTBykW0u84')
+# Run the bot with your Discord bot token
+bot.run(os.environ['DISCORD_BOT_TOKEN'])
